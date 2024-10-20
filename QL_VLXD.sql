bbf611f7e3b0f05986e8b473ce7c60d4229ff70f
davidnguyennq <nguyennhatquan.dev@gmail.com>
2024-10-20 10:49:43 +0700

    CRUD (Loai + NCC) + Fix CRUD HangHoa

---
+++ QL_VLXD.sql
@@ -0,0 +1,275 @@
+﻿CREATE DATABASE QuanLy_VLXD;
+GO
+USE QuanLy_VLXD;
+GO
+
+-- Create QL_NhomNguoiDung table first
+CREATE TABLE QL_NhomNguoiDung (
+    MaNhom VARCHAR(20) PRIMARY KEY,
+    TenNhom NVARCHAR(50) ,
+    GhiChu NVARCHAR(200)
+);
+GO
+
+-- Create DM_ManHinh table
+CREATE TABLE DM_ManHinh (
+    MaManHinh NVARCHAR(50) PRIMARY KEY,
+    TenManHinh NVARCHAR(50)
+);
+GO
+
+-- Create QL_PhanQuyen table
+CREATE TABLE QL_PhanQuyen (
+    MaNhomNguoiDung VARCHAR(20),
+    MaManHinh NVARCHAR(50),
+    CoQuyen BIT,
+    PRIMARY KEY (MaNhomNguoiDung, MaManHinh),
+    FOREIGN KEY (MaNhomNguoiDung) REFERENCES QL_NhomNguoiDung(MaNhom),
+    FOREIGN KEY (MaManHinh) REFERENCES DM_ManHinh(MaManHinh)
+);
+GO
+-- Create NhanVien table
+CREATE TABLE NhanVien (
+    MaNV VARCHAR(10) PRIMARY KEY ,
+    TenNV NVARCHAR(50),
+    GioiTinh NVARCHAR(5),
+    NgaySinh DATETIME,
+    DiaChi NVARCHAR(50),
+    SDT NVARCHAR(15),
+    MatKhau NVARCHAR(30),
+);
+GO
+
+-- Create QL_NguoiDungNhomNguoiDung table
+CREATE TABLE QL_NguoiDungNhomNguoiDung (
+    MaNV VARCHAR(10) NOT NULL ,
+    MaNhomNguoiDung VARCHAR(20) ,
+    GhiChu NVARCHAR(200),
+    PRIMARY KEY (MaNV, MaNhomNguoiDung),
+    FOREIGN KEY (MaNV) REFERENCES NhanVien(MaNV),
+    FOREIGN KEY (MaNhomNguoiDung) REFERENCES QL_NhomNguoiDung(MaNhom)
+);
+GO
+
+-- Create KhachHang table
+CREATE TABLE KhachHang (
+    MaKH VARCHAR(10) PRIMARY KEY,
+    HoTen NVARCHAR(100),
+    NgaySinh DATE,
+    GioiTinh NVARCHAR(10),
+    DienThoai NVARCHAR(15),
+    TaiKhoan NVARCHAR(50),
+    MatKhau NVARCHAR(50),
+    Email NVARCHAR(100),
+    DiaChi NVARCHAR(MAX)
+);
+GO
+
+-- Create NhaCungCap table
+CREATE TABLE NhaCungCap (
+    MaNCC VARCHAR(10) PRIMARY KEY,
+    TenNCC NVARCHAR(50),
+    Diachi NVARCHAR(50),
+    SDT NVARCHAR(15)
+);
+GO
+
+-- Create Loai table
+CREATE TABLE Loai (
+    MaLoai VARCHAR(10) PRIMARY KEY,
+    TenLoai NVARCHAR(50),
+    ThongTin NVARCHAR(100),
+);
+GO
+
+-- Create HangHoa table
+CREATE TABLE HangHoa (
+    MaHH VARCHAR(10) PRIMARY KEY,
+    TenHangHoa NVARCHAR(60),
+    DonVi NVARCHAR(50),
+	SoLuongTon INT,
+    HinhAnh NVARCHAR(255),
+    GiaBan INT,
+    MaLoai VARCHAR(10) FOREIGN KEY REFERENCES Loai(MaLoai),
+    MaNCC VARCHAR(10) FOREIGN KEY REFERENCES NhaCungCap(MaNCC)
+
+);
+GO
+
+-- Tạo bảng DonBanHang
+CREATE TABLE DonBanHang (
+    MaDonBanHang VARCHAR(15) PRIMARY KEY,
+    NgayGiao DATE,
+    NgayDat DATE,
+    NgayThanhToan DATE,
+    MaKH VARCHAR(10),
+    FOREIGN KEY (MaKH) REFERENCES KhachHang(MaKH)
+);
+GO	
+-- Tạo bảng ChiTietDonHang
+CREATE TABLE ChiTietDonBanHang(
+    MaHH VARCHAR(10),
+    MaDonBanHang VARCHAR(15),
+    SoLuong INT,
+    DonGia FLOAT,
+	PRIMARY KEY (MaHH, MaDonBanHang),
+    FOREIGN KEY (MaHH) REFERENCES HangHoa(MaHH),
+    FOREIGN KEY (MaDonBanHang) REFERENCES DonBanHang(MaDonBanHang)
+);
+GO	
+
+-- Create DonNhapHang table
+CREATE TABLE DonNhapHang (
+    MaDonNhapHang VARCHAR(15) PRIMARY KEY,
+    NgayNhap DATE,
+	MaNV VARCHAR(10),
+	FOREIGN KEY (MaNV) REFERENCES NhanVien(MaNV)
+);
+GO
+-- Create ChiTietDonNhapHang table
+CREATE TABLE ChiTietDonNhapHang (
+    MaHH VARCHAR(10),
+	MaDonNhapHang VARCHAR(15),
+    SoLuong INT,
+    DonGia FLOAT,
+	PRIMARY KEY (MaHH, MaDonNhapHang),
+	FOREIGN KEY (MaHH) REFERENCES HangHoa(MaHH),
+	FOREIGN KEY (MaDonNhapHang) REFERENCES DonNhapHang(MaDonNhapHang)
+);
+GO
+
+-- Insert into QL_NhomNguoiDung
+INSERT INTO QL_NhomNguoiDung (MaNhom, TenNhom, GhiChu) VALUES
+('ADMIN', 'Admin', N'Quản trị hệ thống'),
+('NVBCTK', 'NhanVienBaoCaoThongKe', N'Báo cáo - thống kê'),
+('NVBANHANG', 'Sales', N'Nhân viên bán hàng');
+GO
+-- Insert into DM_ManHinh
+INSERT INTO DM_ManHinh (MaManHinh, TenManHinh) VALUES
+('MH_HETHONG', N'Hệ thống'),
+('MH_NVKH', N'Nhân Viên - Khách Hàng'),
+('MH_DM', N'Danh Mục'),
+('MH_NX', N'Nhập - Xuất'),
+('MH_BCTK', N'Báo Cáo Thống Kê'),
+('MH_PQ', N'Phân Quyền'),
+('MH_HT', N'Hỗ Trợ');
+GO
+-- Insert into QL_PhanQuyen
+INSERT INTO QL_PhanQuyen (MaNhomNguoiDung, MaManHinh, CoQuyen) VALUES
+('ADMIN', 'MH_HETHONG', 1),
+('ADMIN', 'MH_PQ', 1);
+GO
+-- Insert into NhanVien
+INSERT INTO NhanVien (MaNV, TenNV, GioiTinh, NgaySinh, DiaChi, SDT, MatKhau) VALUES
+('NV001', 'Nguyen Van A', 'Nam', '1990-01-01', '123 Le Loi', '0123456789',  'pass1'),
+('NV002', 'Tran Thi B', 'Nu', '1992-02-02', '456 Hai Ba Trung', '0987654321',  'pass2'),
+('NV003', 'Le Van C', 'Nam', '1985-03-03', '789 Nguyen Trai', '0912345678',  'pass3'),
+('NV004', 'Pham Thi D', 'Nu', '1988-04-04', '1011 Tran Hung Dao', '0908765432',  'pass4'),
+('NV005', 'Hoang Van E', 'Nam', '1980-05-05', '1213 Vo Thi Sau', '0934567890',  'pass5'),
+('NV006', 'Nguyen Thi F', 'Nu', '1995-06-06', '1415 Ly Thuong Kiet', '0945678901',  'pass6'),
+('NV007', 'Le Thi G', 'Nu', '1982-07-07', '1617 Ba Trieu', '0956789012', 'pass7');
+GO
+-- Insert into QL_NguoiDungNhomNguoiDung
+INSERT INTO QL_NguoiDungNhomNguoiDung (MaNV, MaNhomNguoiDung, GhiChu) VALUES
+('NV001', 'ADMIN', N'Quản trị hệ thống'),
+('NV002', 'NVBANHANG', N'Nhân viên bán hàng'),
+('NV003', 'NVBANHANG', N'Nhân viên bán hàng'),
+('NV004', 'NVBANHANG', N'Nhân viên bán hàng'),
+('NV005', 'NVBANHANG', N'Nhân viên bán hàng');
+GO
+-- Insert into KhachHang
+INSERT INTO KhachHang (MaKH, HoTen, NgaySinh, GioiTinh, DienThoai, TaiKhoan, MatKhau, Email, DiaChi) VALUES
+('KH001', 'Nguyen Van H', '1980-01-01', 'Nam', '0901234567', 'nvh', 'passkh1', 'nvh@gmail.com', '123 Tan Binh'),
+('KH002', 'Tran Thi I', '1985-02-02', 'Nu', '0912345678', 'tti', 'passkh2', 'tti@gmail.com', '456 Binh Thanh'),
+('KH003', 'Le Van J', '1990-03-03', 'Nam', '0923456789', 'lvj', 'passkh3', 'lvj@gmail.com', '789 Thu Duc'),
+('KH004', 'Pham Thi K', '1995-04-04', 'Nu', '0934567890', 'ptk', 'passkh4', 'ptk@gmail.com', '1011 Go Vap'),
+('KH005', 'Hoang Van L', '2000-05-05', 'Nam', '0945678901', 'hvl', 'passkh5', 'hvl@gmail.com', '1213 Phu Nhuan'),
+('KH006', 'Nguyen Thi M', '1985-06-06', 'Nu', '0956789012', 'ntm', 'passkh6', 'ntm@gmail.com', '1415 Binh Chanh'),
+('KH007', 'Le Thi N', '1990-07-07', 'Nu', '0967890123', 'ltn', 'passkh7', 'ltn@gmail.com', '1617 District 1');
+GO
+-- Insert into NhaCungCap
+INSERT INTO NhaCungCap (MaNCC, TenNCC, Diachi, SDT) VALUES
+('NCC001', 'Cty Vat Lieu Xay Dung A', '123 Cong Hoa', '0901122334'),
+('NCC002', 'Cty Vat Lieu Xay Dung B', '456 Le Van Sy', '0902233445'),
+('NCC003', 'Cty Vat Lieu Xay Dung C', '789 Nguyen Kiem', '0903344556'),
+('NCC004', 'Cty Vat Lieu Xay Dung D', '1011 Phan Xich Long', '0904455667'),
+('NCC005', 'Cty Vat Lieu Xay Dung E', '1213 Phan Dinh Phung', '0905566778'),
+('NCC006', 'Cty Vat Lieu Xay Dung F', '1415 Nguyen Van Troi', '0906677889'),
+('NCC007', 'Cty Vat Lieu Xay Dung G', '1617 Nam Ky Khoi Nghia', '0907788990');
+GO
+-- Insert into DanhMucSanPham
+INSERT INTO Loai(MaLoai, TenLoai, ThongTin) VALUES
+('DM001', N'Gạch', N'Gạch xây dựng chất lượng cao' ),
+('DM002', N'Xi măng', N'Xi măng Portland'),
+('DM003', N'Cát', N'Cát xây dựng'),
+('DM004', N'Đá', N'Đá xây dựng'),
+('DM005', N'Thép', N'Thép cường lực'),
+('DM006', N'Sơn', N'Sơn tường chất lượng'),
+('DM007', N'Kính', N'Kính chịu lực');
+GO
+-- Insert into HangHoa
+INSERT INTO HangHoa (MaHH, TenHangHoa, DonVi, SoLuongTon, HinhAnh, GiaBan, MaLoai, MaNCC) VALUES
+('HH001', N'Xi Măng Xây Tô', N'Bao', 200, 'ximang_1.jpg', 85000, 'DM002', 'NCC002'),
+('HH002', N'Xi Măng Fico', N'Bao', 300, 'ximang_2.jpg', 90000, 'DM002', 'NCC002'),
+('HH003', N'Xi Măng Thăng Long', N'Bao', 400, 'ximang_3.jpg', 50000, 'DM002', 'NCC002'),
+('HH004', N'Xi Măng Insee Đa Dụng', N'Bao', 200, 'ximang_4.jpg', 45000, 'DM002', 'NCC002'),
+('HH005', N'Xi Măng Hà Tiên 1', N'Bao', 300, 'ximang_5.jpg', 80000, 'DM002', 'NCC002'),
+('HH006', N'Xi Măng Hà TIên Miền Nam', N'Bao', 400, 'ximang_6.jpg', 750000, 'DM002', 'NCC002'),
+('HH007', N'Xi Măng Xây Tô Hà Tiên 1', N'Bao', 100, 'ximang_7.jpg', 70000, 'DM002', 'NCC002'),
+('HH008', N'Gạch Phước Thành', N'Viên', 200, 'gach_8.jpg', 50000, 'DM001', 'NCC001'),
+('HH009', N'Gạch Thành Tâm', N'Viên', 300, 'gach_9.jpg', 70000, 'DM001', 'NCC001'),
+('HH0010', N'Gạch Tám Quỳnh', N'Viên', 400, 'gach_10.jpg', 73000, 'DM001', 'NCC001'),
+('HH0011', N'Cát bê tông hạt to ', N'M3', 310, 'cat_11.jpg', 320000, 'DM003', 'NCC003'),
+('HH0012', N'Cát bê tông trộn', N'M3', 326, 'cat_12.jpg', 210000, 'DM003', 'NCC003'),
+('HH0013', N'Cát san lấp', N'M3', 223, 'cat_13.jpg', 140000, 'DM003', 'NCC003'),
+('HH0014', N'Cát xây tô', N'M3', 143, 'cat_14.jpg', 170000, 'DM003', 'NCC003'),
+('HH0015', N'Đá 0 X 4 Xanh', N'M3', 200, 'da_15.jpg', 280000, 'DM004', 'NCC004'),
+('HH0016', N'Đá 0 X 4 Xám Đen', N'M3', 200, 'da_16.jpg', 210000, 'DM004', 'NCC004'),
+('HH0017', N'Đá 1 X 2 Xanh', N'M3', 200, 'da_17.jpg', 350000, 'DM004', 'NCC004'),
+('HH0018', N'Đá 4 X 6 Xanh', N'M3', 200, 'da_18.jpg', 300000, 'DM004', 'NCC004'),
+('HH0019', N'Thép Tấm', N'Cây', 100, 'thep_19.jpg', 200000, 'DM005', 'NCC005'),
+('HH0020', N'Thép Hình', N'Cây', 100, 'thep_20.jpg', 200000, 'DM005', 'NCC005'),
+('HH0021', N'Sơn Chống Thâm', N'Thùng', 250, 'son_21.jpg', 430000, 'DM006', 'NCC006'),
+('HH0022', N'Sơn Tường MyColor', N'Thùng', 350, 'son_22.jpg', 230000, 'DM006', 'NCC006'),
+('HH0023', N'Kính G', N'Tấm', 130, 'kinh_23.jpg', 111000, 'DM007', 'NCC007');
+GO
+-- Insert into DonBanHang
+INSERT INTO DonBanHang (MaDonBanHang, NgayGiao, NgayDat, NgayThanhToan, MaKH) VALUES
+('DBH001', '2023-01-01', '2022-12-30', '2023-01-01', 'KH001'),
+('DBH002', '2023-01-02', '2022-12-31', '2023-01-02', 'KH002'),
+('DBH003', '2023-01-03', '2022-12-31', '2023-01-03', 'KH003'),
+('DBH004', '2023-01-04', '2023-01-01', '2023-01-04', 'KH004'),
+('DBH005', '2023-01-05', '2023-01-01', '2023-01-05', 'KH005'),
+('DBH006', '2023-01-06', '2023-01-02', '2023-01-06', 'KH006'),
+('DBH007', '2023-01-07', '2023-01-02', '2023-01-07', 'KH007');
+GO
+-- Insert into ChiTietDonBanHang
+INSERT INTO ChiTietDonBanHang (MaHH, MaDonBanHang, SoLuong, DonGia) VALUES
+('HH001', 'DBH001', 100, 5000),
+('HH002', 'DBH002', 50, 80000),
+('HH003', 'DBH003', 20, 100000),
+('HH004', 'DBH004', 10, 150000),
+('HH005', 'DBH005', 5, 200000),
+('HH006', 'DBH006', 2, 400000),
+('HH007', 'DBH007', 1, 1000000);
+GO
+-- Insert into DonNhapHang
+INSERT INTO DonNhapHang (MaDonNhapHang, NgayNhap, MaNV) VALUES
+('DNH001', '2023-01-01', 'NV001'),
+('DNH002', '2023-01-02', 'NV002'),
+('DNH003', '2023-01-03', 'NV003'),
+('DNH004', '2023-01-04', 'NV004'),
+('DNH005', '2023-01-05', 'NV005'),
+('DNH006', '2023-01-06', 'NV006'),
+('DNH007', '2023-01-07', 'NV007');
+GO
+-- Insert into ChiTietDonNhapHang
+INSERT INTO ChiTietDonNhapHang (MaHH, MaDonNhapHang, SoLuong, DonGia) VALUES
+('HH001', 'DNH001', 100, 4500),
+('HH002', 'DNH002', 50, 75000),
+('HH003', 'DNH003', 20, 95000),
+('HH004', 'DNH004', 10, 140000),
+('HH005', 'DNH005', 5, 190000),
+('HH006', 'DNH006', 2, 380000),
+('HH007', 'DNH007', 1, 950000);
+GO
--- WEB_BMS/WEB_BMS/Controllers/NhanVienController.cs
+++ WEB_BMS/WEB_BMS/Controllers/NhanVienController.cs
@@ -37,35 +37,56 @@ namespace WEB_BMS.Controllers
         [HttpPost]
         public ActionResult ThemSPMoi(HangHoa s, HttpPostedFileBase fileupload)
         {
+            // Tải danh sách loại và nhà cung cấp
             ViewBag.MaLoai = new SelectList(data.Loais.ToList().OrderBy(n => n.TenLoai), "MaLoai", "TenLoai");
             ViewBag.MaNCC = new SelectList(data.NhaCungCaps.ToList().OrderBy(n => n.TenNCC), "MaNCC", "TenNCC");
 
+            // Kiểm tra xem mã sản phẩm có bị trùng không
+            var existingSP = data.HangHoas.FirstOrDefault(h => h.MaHH == s.MaHH);
+            if (existingSP != null)
+            {
+                // Nếu trùng mã sản phẩm, hiển thị thông báo lỗi bằng alert
+                ViewBag.ThongBao1 = "<script>alert('Mã sản phẩm đã tồn tại!');</script>";
+                return View(s); // Trả về view với dữ liệu đã nhập
+            }
+
+            // Kiểm tra ảnh
             if (fileupload == null)
             {
                 ViewBag.Thongbao = "Vui lòng chọn ảnh";
-                return View();
+                return View(s); // Trả về view với dữ liệu đã nhập
             }
-            else
+
+            if (ModelState.IsValid)
             {
-                if (ModelState.IsValid)
+                var filename = Path.GetFileName(fileupload.FileName);
+                var path = Path.Combine(Server.MapPath("~/assets/img/img_product"), filename);
+
+                if (System.IO.File.Exists(path))
                 {
-                    var filename = Path.GetFileName(fileupload.FileName);
-                    var path = Path.Combine(Server.MapPath("~/assets/img/img_product"), filename);
-                    if (System.IO.File.Exists(path))
-                    {
-                        ViewBag.Thongbao = "Ảnh đã tồn tại";
-                    }
-                    else
-                    {
-                        fileupload.SaveAs(path);
-                    }
-                    s.HinhAnh = filename;
-                    data.HangHoas.InsertOnSubmit(s);
-                    data.SubmitChanges();
+                    ViewBag.Thongbao = "Ảnh đã tồn tại";
+                    return View(s); // Nếu ảnh đã tồn tại, trả về view và thông báo
                 }
-                return RedirectToAction("HienThiDSSP");
+                else
+                {
+                    // Lưu ảnh vào thư mục
+                    fileupload.SaveAs(path);
+                }
+
+                // Lưu tên ảnh vào cơ sở dữ liệu
+                s.HinhAnh = filename;
+
+                // Thêm sản phẩm mới vào cơ sở dữ liệu
+                data.HangHoas.InsertOnSubmit(s);
+                data.SubmitChanges();
+
+                return RedirectToAction("HienThiDSSP"); // Chuyển hướng về danh sách sản phẩm sau khi thêm thành công
             }
+
+            return View(s);
         }
+
+
 
         //SuaSP
         public ActionResult SuaSP(string id)
@@ -90,46 +111,45 @@ namespace WEB_BMS.Controllers
             ViewBag.MaLoai = new SelectList(data.Loais.ToList().OrderBy(n => n.TenLoai), "MaLoai", "TenLoai");
             ViewBag.MaNCC = new SelectList(data.NhaCungCaps.ToList().OrderBy(n => n.TenNCC), "MaNCC", "TenNCC");
 
-            if (fileupload == null)
+            // Kiểm tra nếu SoLuongTon là âm
+            if (sp.SoLuongTon < 0)
             {
-                ViewBag.Thongbao = "Vui lòng chọn ảnh bìa";
-                return View();
+                ViewBag.Thongbao = "Số lượng tồn không thể là số âm.";
+                return View(sp); // Trả về view với model
             }
-            else
+
+            HangHoa sph = data.HangHoas.FirstOrDefault(t => t.MaHH == sp.MaHH);
+            if (sph == null)
             {
-                if (ModelState.IsValid)
-                {
-                    var filename = Path.GetFileName(fileupload.FileName);
-                    var path = Path.Combine(Server.MapPath("~/assets/img/img_product"), filename);
-
-                    if (System.IO.File.Exists(path))
-                    {
-                        ViewBag.Thongbao = "Ảnh đã tồn tại";
-                    }
-                    else
-                    {
-                        fileupload.SaveAs(path);
-                    }
-
-                    HangHoa sph = data.HangHoas.FirstOrDefault(t => t.MaHH == sp.MaHH);
-                    if (sph != null)
-                    {
-                        sph.TenHangHoa = sp.TenHangHoa;
-                        sph.DonVi = sp.DonVi;
-                        sph.SoLuongTon = sp.SoLuongTon;
-                        sph.HinhAnh = filename;
-                        sph.GiaBan = sp.GiaBan;
-                        sph.MaLoai = sp.MaLoai;
-                        sph.MaNCC = sp.MaNCC;
-
-
-                        data.SubmitChanges();
-                    }
-                }
-
-                return RedirectToAction("HienThiDSSP");
+                ViewBag.Thongbao = "Sản phẩm không tồn tại.";
+                return View(sp); // Trả về view với model
             }
+
+            // Nếu không có file upload, giữ nguyên hình ảnh hiện tại
+            if (fileupload != null)
+            {
+                var filename = Path.GetFileName(fileupload.FileName);
+                var path = Path.Combine(Server.MapPath("~/assets/img/img_product"), filename);
+
+                // Lưu hình ảnh mới
+                fileupload.SaveAs(path);
+                sph.HinhAnh = filename; // Cập nhật hình ảnh
+            }
+            // Nếu không có file upload, giữ nguyên hình ảnh hiện tại
+
+            // Cập nhật các thuộc tính khác
+            sph.TenHangHoa = sp.TenHangHoa;
+            sph.DonVi = sp.DonVi;
+            sph.SoLuongTon = sp.SoLuongTon; // Cập nhật số lượng tồn
+            sph.GiaBan = sp.GiaBan;
+            sph.MaLoai = sp.MaLoai;
+            sph.MaNCC = sp.MaNCC;
+
+            data.SubmitChanges();
+
+            return RedirectToAction("HienThiDSSP");
         }
+
         //XoaSP
         public ActionResult XoaSP(string id)
         {
@@ -167,6 +187,169 @@ namespace WEB_BMS.Controllers
             }
             ViewBag.tb = "Sản phẩm đang có trong đơn nhập hàng hoặc bán hàng. Vui lòng kiểm tra lại!!!";
             return View(hh);
+        }
+
+
+        ///CRUD LOAI
+        public ActionResult HienThiDSLoai()
+        {
+            List<Loai> dsLoai = data.Loais.ToList();
+            return View(dsLoai);
+        }
+
+        [HttpGet]
+        public ActionResult ThemLoaiMoi()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult ThemLoaiMoi(Loai loai)
+        {
+            // Kiểm tra xem mã loại có bị trùng không
+            var existingLoai = data.Loais.FirstOrDefault(l => l.MaLoai == loai.MaLoai);
+            if (existingLoai != null)
+            {
+                // Nếu trùng mã loại, hiển thị thông báo lỗi bằng alert
+                ViewBag.ThongBao = "<script>alert('Mã loại đã tồn tại!');</script>";
+                return View(loai); // Trả về view với dữ liệu đã nhập
+            }
+
+            if (ModelState.IsValid)
+            {
+                // Thêm loại mới vào cơ sở dữ liệu
+                data.Loais.InsertOnSubmit(loai);
+                data.SubmitChanges();
+
+                return RedirectToAction("HienThiDSLoai"); // Chuyển hướng về danh sách loại sau khi thêm thành công
+            }
+
+            return View(loai); // Trả về view trong trường hợp ModelState không hợp lệ
+        }
+
+
+        public ActionResult SuaLoai(string id)
+        {
+            Loai loai = data.Loais.SingleOrDefault(n => n.MaLoai == id);
+            if (loai == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+            return View(loai);
+        }
+
+        [HttpPost]
+        public ActionResult SuaLoai(Loai loai)
+        {
+            if (ModelState.IsValid)
+            {
+                Loai loaiCu = data.Loais.FirstOrDefault(l => l.MaLoai == loai.MaLoai);
+                if (loaiCu != null)
+                {
+                    loaiCu.TenLoai = loai.TenLoai;
+                    loaiCu.ThongTin = loai.ThongTin;
+                    data.SubmitChanges();
+                }
+                return RedirectToAction("HienThiDSLoai");
+            }
+            return View(loai);
+        }
+
+        public ActionResult XoaLoai(string id)
+        {
+            Loai l = data.Loais.FirstOrDefault(t => t.MaLoai == id);
+
+            if (l.HangHoas.Count == 0)
+            {
+                data.Loais.DeleteOnSubmit(l);
+                data.SubmitChanges();
+                return RedirectToAction("HienThiDSLoai");
+            }
+            ViewBag.tb = "Loại này đang có sản phẩm, Không xóa được!!!";
+            return View(l);
+        }
+
+        ///CRUD NHA CUNG CAP
+        public ActionResult HienThiDSNCC()
+        {
+            List<NhaCungCap> dsNCC = data.NhaCungCaps.ToList();
+            return View(dsNCC);
+        }
+
+        [HttpGet]
+        public ActionResult ThemNCCMoi()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult ThemNCCMoi(NhaCungCap ncc)
+        {
+            // Kiểm tra xem mã NCC có bị trùng không
+            var existingNCC = data.NhaCungCaps.FirstOrDefault(n => n.MaNCC == ncc.MaNCC);
+
+            if (existingNCC != null)
+            {
+                // Nếu trùng mã NCC, hiển thị thông báo lỗi bằng alert
+                ViewBag.ThongBao = "<script>alert('Mã Nhà Cung Cấp đã tồn tại!');</script>";
+                return View(ncc); // Trả về view với dữ liệu đã nhập
+            }
+
+            // Nếu không trùng, tiếp tục thêm mới
+            if (ModelState.IsValid)
+            {
+                data.NhaCungCaps.InsertOnSubmit(ncc);
+                data.SubmitChanges();
+                return RedirectToAction("HienThiDSNCC"); // Chuyển hướng về danh sách NCC sau khi thêm thành công
+            }
+
+            return View(ncc);
+        }
+
+
+        public ActionResult SuaNCC(string id)
+        {
+            NhaCungCap ncc = data.NhaCungCaps.SingleOrDefault(n => n.MaNCC == id);
+            if (ncc == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+            return View(ncc);
+        }
+
+        [HttpPost]
+        public ActionResult SuaNCC(NhaCungCap ncc)
+        {
+            if (ModelState.IsValid)
+            {
+                NhaCungCap nccCu = data.NhaCungCaps.FirstOrDefault(n => n.MaNCC == ncc.MaNCC);
+                if (nccCu != null)
+                {
+                    nccCu.TenNCC = ncc.TenNCC;
+                    nccCu.Diachi = ncc.Diachi;
+                    nccCu.SDT = ncc.SDT;
+                    data.SubmitChanges();
+                }
+                return RedirectToAction("HienThiDSNCC");
+            }
+            return View(ncc);
+        }
+
+        public ActionResult XoaNCC(string id)
+        {
+            NhaCungCap l = data.NhaCungCaps.FirstOrDefault(t => t.MaNCC == id);
+
+            if (l.HangHoas.Count == 0)
+            {
+                data.NhaCungCaps.DeleteOnSubmit(l);
+                data.SubmitChanges();
+                return RedirectToAction("HienThiDSNCC");
+            }
+            ViewBag.tb = "Nhà cung cấp này hiện vẫn đang cung cấp sản phẩm. Không xóa được!!!";
+            return View(l);
         }
     }
 }
